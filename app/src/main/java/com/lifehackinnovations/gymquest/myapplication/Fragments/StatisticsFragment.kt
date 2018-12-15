78e932f1592c49e6dee42c76d4e98fb1acc388e0
Patrick <patrickmwatson@gmail.com>
2018-12-15 15:24:22 -0500

    Added clickability to statistics screen class select button

--- app/src/main/java/com/lifehackinnovations/gymquest/myapplication/Fragments/StatisticsFragment.kt
+++ app/src/main/java/com/lifehackinnovations/gymquest/myapplication/Fragments/StatisticsFragment.kt
@@ -3,11 +3,16 @@ package com.lifehackinnovations.gymquest.myapplication.Fragments
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lifehackinnovations.gymquest.myapplication.MainActivity
+import com.lifehackinnovations.gymquest.myapplication.MainActivity.Companion.replaceFragment
 import com.lifehackinnovations.gymquest.myapplication.R
 import com.lifehackinnovations.gymquest.myapplication.databinding.FragmentStatisticsBinding
+import kotlinx.android.synthetic.main.fragment_class_description.view.*
+import kotlinx.android.synthetic.main.fragment_welcome_back.view.*
 
 class StatisticsFragment: Fragment() {
 
@@ -16,7 +21,10 @@ class StatisticsFragment: Fragment() {
         val binding: FragmentStatisticsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_statistics, container, false)
         val myView: View = binding.root
 
-
+        myView.select_class_button.setOnClickListener { view ->
+            val selectYourTrainingFragment = SelectYourTrainingFragment();
+            (activity as MainActivity).replaceFragment(selectYourTrainingFragment, R.id.fragment_container)
+        }
 
         return myView
     }
